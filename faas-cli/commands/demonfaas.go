b478843af46a8eef745c3da0b8de3d8892a1a427
Moealik <moegame1123@gmail.com>
2024-11-19 16:16:47 -0500

    MAK: added laptop work. Working on getting packages/library imports working

--- faas-cli/commands/demonfaas.go
+++ faas-cli/commands/demonfaas.go
@@ -25,37 +25,44 @@ func d_assert(predicate bool) {
 }
 
 var (
-	flag1 int
-	flag2 bool
+	dockerFile string
 )
 
 func init() {
 	d_print("demonfaas init")
 
-	demonfaasCmd.Flags().IntVarP(&flag1, "flag1", "1", 0, "flag1 test")
-	demonfaasCmd.Flags().BoolVarP(&flag2, "flag2", "2", false, "flag2 test")
+	// demonfaasCmd.Flags().IntVarP(&flag1, "flag1", "1", 0, "flag1 test")
+	// demonfaasCmd.Flags().BoolVarP(&flag2, "flag2", "2", false, "flag2 test")
+	// src, dockerfile, deployment.yaml (already delpoyed stuff to k8)
+
+	demonfaasCmd.Flags().StringVar(&dockerFile, "dockerfile", "", "original input dockerfile")
 
 	faasCmd.AddCommand(demonfaasCmd)
 }
 
 var demonfaasCmd = &cobra.Command{
 	Use:      `demonfaas`, // needs to match file name
-	Short:    `fill me in`,
-	Long:     `fill me in`,
-	Example:  `fill me in`,
+	Short:    `short fill me in`,
+	Long:     `long fill me in`,
+	Example:  `example fill me in`,
 	PreRunE:  preRun,
 	RunE:     run,
 	PostRunE: postRun,
 }
 
 func preRun(cmd *cobra.Command, args []string) error {
+	d_print("pre run")
+
+	d_print(dockerFile)
 	return nil
 }
 
 func run(cmd *cobra.Command, args []string) error {
+	d_print("run")
 	return nil
 }
 
 func postRun(cmd *cobra.Command, args []string) error {
+	d_print("post run")
 	return nil
 }
